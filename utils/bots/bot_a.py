74c97036b072ed926e5358c1eb71ae90e4724f8d
HippolyteLC <152972861+HippolyteLC@users.noreply.github.com>
2024-01-27 17:54:47 +0100

    Update bot_a.py
    simplified BotA as it was used in the experimental setup

--- utils/bots/bot_a.py
+++ utils/bots/bot_a.py
@@ -6,33 +6,15 @@ class BotA(Bot):
 
     def get_move(self, perspective: PlayerPerspective, leader_move: Optional[Move]) -> Move:
         valid_moves = [move for move in perspective.valid_moves()]
-        # returns the first marriage move in valid moves
-        for move in valid_moves:
-            if move.is_marriage():
-                return move
-        # returns the first trump exchange move in valid moves
-        for move in valid_moves:
-            if move.is_trump_exchange():
-                return move
 
-        non_trumps = [move for move in valid_moves if move.card.suit != perspective.get_trump_suit()]
+        # point scorer method but shortened for ease of use
         rank_to_points = SchnapsenTrickScorer().rank_to_points
 
         # IF FOLLOWER MOVES:
-        if not perspective.am_i_leader():
-            # here we play to lose the last trick before phase 2
-            # if perspective.get_talon_size() == 2:
-            #     non_trumps_sorted = sorted(non_trumps, key= lambda move: rank_to_points(move.cards[0].rank))    
-            #     for move in non_trumps_sorted:
-            #         if rank_to_points(move.cards[0].rank) < rank_to_points(leader_move.cards[0].rank):
-            #             return move
-            #         if move.cards[0].suit != leader_move.cards[0].suit:
-            #             return move      
+        if not perspective.am_i_leader():  
             # here we return the lowest value card 
             return min(valid_moves, key= lambda move: rank_to_points(move.cards[0].rank))       
 
         # IF LEADER MOVES:
-        # if perspective.get_talon_size() == 2:
-        #         return min(non_trumps, key= lambda move: rank_to_points(move.cards[0].rank))
-        
+        # here we return the highest ranking card
         return max(valid_moves, key=lambda move: rank_to_points(move.cards[0].rank))
